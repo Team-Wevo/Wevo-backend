d6725aedc331d5e7aec29d6ef978835ae72387b2
JinYoung Shin <sjyoungzzang@gmail.com>
2026-06-27 00:22:29 +0900

    chore: 프로젝트 초기 설정 및 도메인 구조 세팅 (#2)

---
+++ Wevo-backend/src/main/java/com/wevo/backend/WevoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wevo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WevoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WevoBackendApplication.class, args);
+	}
+
+}
